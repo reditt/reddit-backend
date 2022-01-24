f3e67b5d836fa706d61465565cdf5befad68ef48
Rohit Varsada <varsadarohit@gmail.com>
2022-01-24 13:12:29 +0530

    added ssl

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRouter = require("./src/routers/user.routes");
-// const db = require("./src/config/db.config");
-// require("./src/config/db.config");
+const db = require("./src/config/db.config");
+require("./src/config/db.config");
 const app = express();
 require("dotenv").config();
 
 app.use(cors("*"));
 app.use(bodyParser.json());
 
-// app.use("/api", userRouter);
+app.use("/api", userRouter);
 
-// db.sequelize.sync();
+db.sequelize.sync();
 
 app.get("/", (req, res) => res.send("message" + process.env.Name));
 
