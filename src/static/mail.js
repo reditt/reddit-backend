a91fa40b7d6b7de66caa616fcf7ff9d23e4a3eb8
Rohit Varsada <varsadarohit@gmail.com>
2022-01-27 13:17:10 +0530

    updated reset password url

--- src/static/mail.js
+++ src/static/mail.js
@@ -50,7 +50,7 @@ exports.mailTemplate = (token) => {
                                             password has been generated for you. To reset your password, click the
                                             following link and follow the instructions.
                                         </p>
-                                        <a href="http://localhost:3000/${token}"
+                                        <a href="http://localhost:3000/resetpassword/${token}"
                                             style="background:#4DACFF;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
                                             Password</a>
                                     </td>
